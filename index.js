9514e1bf8a943c06d0f18ea8237783879651dfe1
FallDetect <htok@nhb-ind.com>
2026-06-19 17:26:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout:   10000,
+  socketTimeout:     15000,
 });
 const db = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
